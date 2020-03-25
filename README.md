10b322e2098bca6c070c96d432e21b39c192584b
Zababurin Sergey <s.zababurin.v@gmail.com>
2020-03-26 02:41:44 +0300

    update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # template
 
 https://stackoverflow.com/questions/60503466/cant-access-source-object-when-window-postmessage-in-edge
+
+https://github.com/wavesplatform/waves-signature-adapter
+
+https://github.com/wavesplatform/waves-transactions
+
+https://github.com/wavesplatform/waves-transactions
--- app.waves.mjs
+++ app.waves.mjs
@@ -2,10 +2,16 @@ import Signer  from '@waves/signer';
 import ProviderSeed  from '@waves/provider-seed';
 import Provider from '@waves.exchange/provider-web';
 import transactions from '@waves/waves-transactions';
+import signatureAdapter from '@waves/signature-adapter';
+import dataEntities from '@waves/data-entities';
+import signatureGenerator from '@waves/signature-generator';
 export default {
         Provider:Provider,
         ProviderSeed:ProviderSeed,
         Signer:Signer,
         transactions:transactions,
         libs:transactions['libs'],
+        signatureAdapter:signatureAdapter,
+        dataEntities:dataEntities,
+        signatureGenerator:signatureGenerator
 }
